0212d221dfbd14253cd412e6d8610b714bcf16a5
heinzrm <heinzr@hotmail.com>
2022-07-08 21:34:15 -0300

    Commit

--- FourTons.py
+++ FourTons.py
@@ -1,10 +1,17 @@
 from bs4 import BeautifulSoup
+from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
+import shutil
 import requests
-import zipfile as zipcompact
+import zipfile
+import os
+import re
+import wget
 
 URL = 'http://www.4tons.com.br/03discos.htm'
 allowed_domains = 'http://www.4tons.com.br'
-headers = {'User_agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
+headers = {'User_agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                         "(KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
 
 site = requests.get(URL, headers=headers)
 soup = BeautifulSoup(site.content, 'html.parser')
@@ -12,23 +19,66 @@ table = soup.body.find_all('table', class_='MsoTableGrid')
 
 Dados = []
 for tabela in table:
-    registros = tabela.find_all('p')
-    for ref in registros:
-        if ref.a and ref.a.get('href') != None and ref.a.get('href') != '#KINGS' and ref.a.get(
+    Registros = tabela.find_all('p')
+    for ref in Registros:
+        if ref.a and ref.a.get('href') is not None and ref.a.get('href') != '#KINGS' and ref.a.get(
                 'href') != '#TOPO' and ref.span.text != '':
-            url = allowed_domains + '/' + ref.a.get('href').replace('..', '')
+            url = allowed_domains + '/' + ref.a.get('href').replace('..', '').replace(r"%20", " ")
             album = ref.span.text.replace('TOPO', '').replace('\r\n', '').strip()
             Dados.append([album, url])
-            resposta = requests.get(url)
-            if resposta.status_code == requests.codes.OK:
+            # respostas = requests.get(url)
+            try:
+                resposta = wget.download(url)
                 with open(album + '.zip', 'wb') as novo_arquivo:
                     novo_arquivo.write(resposta.content)
                 print("Download finalizado. Salvo em: {}".format(album + '.zip'))
-            else:
-                resposta.raise_for_status()
+            except Exception as e:
+                print(r"Falha ao baixar o arquivo: {}".format(album + r".zip"))
 
-            zf = zipcompact.ZipFile(album + '.zip')
-            list = zf.namelist()
+            my_dir = r"D:\heinz\Music\The King's Heralds\Teste"
+            if not os.path.exists(album + '.zip'):
+                continue
 
-            for arquivo in list:
-                zf.extract(arquivo, r'd:\test"\"' + arquivo)
+            try:
+                with zipfile.ZipFile(album + '.zip') as filezip:
+                    if filezip.testzip() is not None:
+                        os.remove(album + '.zip')
+                        continue
+
+                    for membro in filezip.namelist():
+                        if not membro.endswith(".mp3"):
+                            continue
+                        nomeArquivo = os.path.basename(membro)
+                        if os.path.dirname(membro) == '':
+                            Folder = "The King's Heralds - " + album
+                        else:
+                            Folder = "The King's Heralds - " + os.path.dirname(membro)
+
+                        if not nomeArquivo:
+                            continue
+
+                        folderDestino = my_dir + r"\\" + Folder
+                        if not os.path.isdir(folderDestino):
+                            os.mkdir(folderDestino)
+
+                        source = filezip.open(membro)
+                        target = open(os.path.join(folderDestino, nomeArquivo), "wb")
+                        with source, target:
+                            shutil.copyfileobj(source, target)
+
+                        Musica = MP3(os.path.join(folderDestino, nomeArquivo), ID3=EasyID3)
+                        titulo = re.sub(r"[^a-zA-Z ]", r"", nomeArquivo.replace('.mp3', ''))
+                        Musica['title'] = titulo
+                        Musica['album'] = os.path.dirname(membro)
+                        Musica['artist'] = r"The King's Heralds"
+                        Musica['albumartist'] = "The King's Heralds"
+                        Musica['genre'] = "Teste"
+                        Musica.save()
+
+                os.remove(album + '.zip')
+                filezip.close()
+            except zipfile.error as ex:
+                os.remove(album + '.zip')
+                continue
+    contador = + 1
+    print(r"Registro {} de {}".format(contador, Registros.count))
