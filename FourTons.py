b391be61494963089f638d1e2d4c1f01f05cd6a9
heinzrm <heinzr@hotmail.com>
2022-07-09 00:15:49 -0300

    Exibir andamento dos downloads

--- FourTons.py
+++ FourTons.py
@@ -18,24 +18,24 @@ soup = BeautifulSoup(site.content, 'html.parser')
 table = soup.body.find_all('table', class_='MsoTableGrid')
 
 Dados = []
+contador = 0
 for tabela in table:
     Registros = tabela.find_all('p')
     for ref in Registros:
+        contador = contador + 1
+        print(r"Registro {} de {}".format(contador, len(Registros)))
         if ref.a and ref.a.get('href') is not None and ref.a.get('href') != '#KINGS' and ref.a.get(
                 'href') != '#TOPO' and ref.span.text != '':
-            url = allowed_domains + '/' + ref.a.get('href').replace('..', '').replace(r"%20", " ")
+            url = allowed_domains + '/' + ref.a.get('href').replace('../', '').replace(r"%20", " ")
             album = ref.span.text.replace('TOPO', '').replace('\r\n', '').strip()
             Dados.append([album, url])
-            # respostas = requests.get(url)
             try:
-                resposta = wget.download(url)
-                with open(album + '.zip', 'wb') as novo_arquivo:
-                    novo_arquivo.write(resposta.content)
+                resposta = wget.download(url, album + r".zip")
                 print("Download finalizado. Salvo em: {}".format(album + '.zip'))
             except Exception as e:
                 print(r"Falha ao baixar o arquivo: {}".format(album + r".zip"))
 
-            my_dir = r"D:\heinz\Music\The King's Heralds\Teste"
+            my_dir = r"C:\Users\Heinz2\Music\The King's Heralds\Teste"
             if not os.path.exists(album + '.zip'):
                 continue
 
@@ -43,6 +43,7 @@ for tabela in table:
                 with zipfile.ZipFile(album + '.zip') as filezip:
                     if filezip.testzip() is not None:
                         os.remove(album + '.zip')
+                        print(r"Erro ao descompactar o arquivo {}".format(album + '.zip'))
                         continue
 
                     for membro in filezip.namelist():
@@ -55,6 +56,8 @@ for tabela in table:
                             Folder = "The King's Heralds - " + os.path.dirname(membro)
 
                         if not nomeArquivo:
+                            os.remove(album + '.zip')
+                            print(r"Erro ao descompactar o arquivo {}".format(album + '.zip'))
                             continue
 
                         folderDestino = my_dir + r"\\" + Folder
@@ -79,6 +82,5 @@ for tabela in table:
                 filezip.close()
             except zipfile.error as ex:
                 os.remove(album + '.zip')
+                print(r"Erro ao descompactar o arquivo {}".format(album + '.zip'))
                 continue
-    contador = + 1
-    print(r"Registro {} de {}".format(contador, Registros.count))
